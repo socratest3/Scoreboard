ec5b25282e408487fe97515b1dd8c134fa2fe2df
socratest3 <socratest3@nycstudents.net>
2024-10-09 18:22:56 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,41 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+    String info;
+    Scoreboard game = new Scoreboard("Red", "Blue");
+    info = game.getScore();
+    System.out.println(info);
+    game.recordPlay(1);
+    info = game.getScore();
+    System.out.println(info);
+
+    game.recordPlay(0);
+    info = game.getScore();
+    System.out.println(info);
+
+    info = game.getScore();
+    System.out.println(info);
+    info = game.getScore();
+    game.recordPlay(3);
+    info = game.getScore();
+    game.recordPlay(1);
+    game.recordPlay(0);
+    System.out.println(info);
+
+    info = game.getScore();
+    game.recordPlay(0);
+    System.out.println(info);
+
+    game.recordPlay(4);
+    game.recordPlay(0);
+    info = game.getScore();
+    System.out.println(info);
+    
+    Scoreboard match = new Scoreboard("Yankees", "Mets");
+    info = match.getScore();
+    System.out.println(info);
+    info = game.getScore();
+    System.out.println(info);
+    }
+}
